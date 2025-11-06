601f674f192c1da13557d400379db55a98f05c63
angelajanebouw <angelabouw@yahoo.es>
2025-11-06 19:29:31 +0100

    Added Vite Lit

---
+++ vite.config.js
@@ -0,0 +1,26 @@
+import {defineConfig} from "vite";
+
+export default defineConfig({
+    build: {
+        outDir: 'dist', // Output folder (relative to project root)
+        emptyOutDir: true, // Clear old files before building
+        sourcemap: false // Optional: generate source maps
+    },
+
+    // Adjust Vites dev server to work with DDEV
+    // https://vitejs.dev/config/server-options.html
+    server: {
+        // Respond to all network requests
+        host: "0.0.0.0",
+        port: 5173,
+        open: true, // Open browser automatically
+        strictPort: true,
+        proxy: {
+            '/api': {
+                target: 'http://localhost:8080',
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/api/, '')
+            }
+        }
+    },
+})
