6bed8e6d6aec182e4cff94d2316582ac9857832e
angelajanebouw <angelabouw@yahoo.es>
2025-11-08 14:19:30 +0100

    Fix resolve

--- vite.config.js
+++ vite.config.js
@@ -18,14 +18,14 @@ export default defineConfig({
 
     // Adjust Vites dev server to work with DDEV
     // https://vitejs.dev/config/server-options.html
+    resolve: {
+        alias: {
+            '/features/feature-home/dist': path.resolve(__dirname, './features/feature-home/dist'),
+            '/features/feature-login/dist': path.resolve(__dirname, './features/feature-login/dist'),
+            '/features/feature-quick-game/dist': path.resolve(__dirname, './features/feature-quick-game/dist'),
+        }
+    },
     server: {
-        resolve: {
-            alias: {
-                '/features/feature-home/dist': path.resolve(__dirname, './features/feature-home/dist'),
-                '/features/feature-login/dist': path.resolve(__dirname, './features/feature-login/dist'),
-                '/features/feature-quick-game/dist': path.resolve(__dirname, './features/feature-quick-game/dist'),
-            }
-        },
         // Respond to all network requests
         host: "0.0.0.0",
         port: 5173,
